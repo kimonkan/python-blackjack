546e11c54b7df663fb4b92671ef156151c5feecc
Kimon Kanellos <kimonkanellos@gmail.com>
2024-09-09 19:29:37 +0300

    Changed Ace value

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import random
 
 suits = ('Hearts', 'Diamonds', 'Clubs', 'Spades')
 ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
-values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':1}
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':11}
 
 class Card():
     def __init__(self, suit, rank):
