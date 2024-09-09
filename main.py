ce4f8183831898c7fff925ec12967ba292570144
Kimon Kanellos <kimonkanellos@gmail.com>
2024-09-09 16:47:27 +0300

    Created class Card and Deck

---
+++ main.py
@@ -0,0 +1,21 @@
+import random
+
+suits = ('Hearts', 'Diamonds', 'Clubs', 'Spades')
+ranks = ('Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
+values = {'Two':2, 'Three':3, 'Four':4, 'Five':5, 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':1}
+
+class Card():
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+
+class Deck:
+
+    def __init__(self):
+
+        self.deck = []
+
+        for suit in suits:
+            for rank in ranks:
+                created_card = Card(suit, rank)
+                self.deck.append(created_card)
